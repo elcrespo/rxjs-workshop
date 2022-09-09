e99479e0864943101f2edbea592979f0477d79da
Carlos Hernandez <carlos.hernandez@kinesso.com>
2022-09-08 20:50:50 -0500

    approach

--- src/app/rxjs-workshop-table/rxjs-workshop-table.component.ts
+++ src/app/rxjs-workshop-table/rxjs-workshop-table.component.ts
@@ -13,10 +13,11 @@ export class RxjsWorkshopTableComponent implements OnInit {
   pageSizeOptions: number[] = [5, 10, 25, 100];
   displayedColumns: string[] = ['select', 'name', 'description','image', 'modified'];
   data: MarvelResults[] = [];
-  resultsLength = 0;
   isLoadingResults = true;
   isRateLimitReached = false;
-
+  results$ = this.rxjsWorkshopTableService.results$;
+  total$ = this.rxjsWorkshopTableService.total$;
+  isLoadingData$ = this.rxjsWorkshopTableService.isLoadingData$;
   constructor(private rxjsWorkshopTableService: RxjsWorkshopTableService) {}
 
   ngOnInit() {
@@ -31,14 +32,17 @@ export class RxjsWorkshopTableComponent implements OnInit {
   applyFilter($event: KeyboardEvent) {
     const filterValue = ($event.target as HTMLInputElement).value;
     console.log('filter', filterValue);
+    this.rxjsWorkshopTableService.setFilter(filterValue);
   }
 
   paginatorEvent($event: PageEvent) {
     console.log('paginatorEvent', $event);
+    this.rxjsWorkshopTableService.setPaginator($event);
   }
 
   onSortChange($event: Sort) {
     console.log('sort change', $event);
+    this.rxjsWorkshopTableService.setSort($event);
   }
 
   selectAll($event: MatCheckboxChange) {
--- src/app/rxjs-workshop-table/rxjs-workshop-table.service.ts
+++ src/app/rxjs-workshop-table/rxjs-workshop-table.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Sort, SortDirection} from "@angular/material/sort";
+import {BehaviorSubject, combineLatest, debounceTime, map, Observable, shareReplay, switchMap, tap} from "rxjs";
+import {Sort} from "@angular/material/sort";
+import {PageEvent} from "@angular/material/paginator";
 
 export interface MarvelResults {
   id: number;
@@ -37,15 +38,70 @@ export interface MarvelQueryParams {
   providedIn: 'root'
 })
 export class RxjsWorkshopTableService {
+  private baseUrl = 'http://gateway.marvel.com/v1/public/';
+  private sortSubject: BehaviorSubject<Sort> = new BehaviorSubject<Sort>({active: 'name', direction: 'asc'});
+  private paginatorSubject: BehaviorSubject<PageEvent> = new BehaviorSubject<PageEvent>({pageIndex: 0, pageSize: 10, length: 20});
+  private filterSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  private isLoadingDataSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  sort$: Observable<Sort> = this.sortSubject.asObservable();
+  paginator$: Observable<PageEvent> = this.paginatorSubject.asObservable();
+  filter$: Observable<string> = this.filterSubject.asObservable().pipe(
+    debounceTime(500)
+  );
+  isLoadingData$ = this.isLoadingDataSubject.asObservable();
+
+  marvelQueryParams$: Observable<MarvelQueryParams> = combineLatest(
+    [this.sort$, this.paginator$, this.filter$]
+  ).pipe(
+    map(([sort, paginator, filter]) => {
+      const nameStartsWith = filter;
+      const offset = paginator.pageIndex * paginator.pageSize;
+      const limit = (paginator.pageSize * paginator.pageIndex) || paginator.pageSize;
+      return {
+        ...(nameStartsWith.length) && {nameStartsWith},
+        offset,
+        limit,
+        sort
+      }
+    })
+  );
+
+  data$: Observable<MarvelApi> = this.marvelQueryParams$.pipe(
+    tap(() => this.isLoadingDataSubject.next(true)),
+    switchMap((queryParams) => this.getMarvelCharacters(queryParams)),
+    tap(() => this.isLoadingDataSubject.next(false)),
+    shareReplay(1)
+  );
+
+  results$: Observable<MarvelResults[]> = this.data$.pipe(
+    map(response => response.data.results)
+  );
+
+  total$: Observable<number> = this.data$.pipe(
+    map(response => response.data.total)
+  );
 
   constructor(private _httpClient: HttpClient) {}
-  baseUrl = 'http://gateway.marvel.com/v1/public/';
 
+  /*Este método pertenecería a otro tipo de servicio*/
   getMarvelCharacters(queryParams: MarvelQueryParams = {}): Observable<MarvelApi> {
     const apikey = '282bf23e6ebfd35ce4499d33d021bb19';
     const { sort, ...mQueryParams} = queryParams;
     const orderBy = sort?.direction === 'desc'? `-${sort.active}` : sort?.active || 'name';
     const requestUrl = `${this.baseUrl}characters`;
     return this._httpClient.get<MarvelApi>(requestUrl, {params: {...mQueryParams, orderBy, apikey}})
+  }
+
+  setSort(sort: Sort) {
+    this.sortSubject.next(sort);
+  }
+
+  setPaginator(paginator: PageEvent) {
+    this.paginatorSubject.next(paginator);
+  }
+
+  setFilter(filter: string) {
+    this.filterSubject.next(filter);
   }
 }
